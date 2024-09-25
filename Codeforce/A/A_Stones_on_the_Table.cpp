14347ca33a6fdcd9e70698cccb366b0479c6f5ae
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2024-09-25 10:02:00 +0530

    Codeforce Solution Added
    A. Stones on the Table Added

---
+++ Codeforce/A/A_Stones_on_the_Table.cpp
@@ -0,0 +1,32 @@
+/*
+Codeforce : A. Stones on the Table
+Author   : Shah Mrunal Nirajkumar
+Date     : 25 September 2024
+*/
+
+#include <bits\stdc++.h>
+
+using namespace std;
+
+void solve(){
+  int stringLength;
+  cin >> stringLength;
+
+  char s[stringLength];
+  cin >> s;
+
+  int numberOfNeighbour = 0;
+
+  for(int i = 0; i < stringLength - 1; i++){
+    if(s[i] == s[i+1]){
+      numberOfNeighbour++;
+    }
+  }
+  cout << numberOfNeighbour << endl;
+}
+
+int main() {
+  solve();
+
+  return 0;
+}
